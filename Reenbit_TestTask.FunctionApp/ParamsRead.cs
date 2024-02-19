afa08b803f82101b5e54b2313682093e0de27510
Andrii Kuziv <happyandreas18@gmail.com>
2024-02-19 19:55:21 +0200

    removed rudimentary code

--- Reenbit_TestTask.FunctionApp/ParamsRead.cs
+++ Reenbit_TestTask.FunctionApp/ParamsRead.cs
@@ -25,41 +25,5 @@ namespace Reenbit_TestTask.FunctionApp
 
             return currentValue;
         }
-
-        //[FunctionName("ParamsRead")]
-        //public static async Task<List<string>> RunOrchestrator(
-        //    [OrchestrationTrigger] IDurableOrchestrationContext context)
-        //{
-        //    var outputs = new List<string>();
-
-        //    // Replace "hello" with the name of your Durable Activity Function.
-        //    outputs.Add(await context.CallActivityAsync<string>(nameof(SayHello), "Tokyo"));
-        //    outputs.Add(await context.CallActivityAsync<string>(nameof(SayHello), "Seattle"));
-        //    outputs.Add(await context.CallActivityAsync<string>(nameof(SayHello), "London"));
-
-        //    // returns ["Hello Tokyo!", "Hello Seattle!", "Hello London!"]
-        //    return outputs;
-        //}
-
-        //[FunctionName(nameof(SayHello))]
-        //public static string SayHello([ActivityTrigger] string name, ILogger log)
-        //{
-        //    log.LogInformation("Saying hello to {name}.", name);
-        //    return $"Hello {name}!";
-        //}
-
-        //[FunctionName("ParamsRead_HttpStart")]
-        //public static async Task<HttpResponseMessage> HttpStart(
-        //    [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestMessage req,
-        //    [DurableClient] IDurableOrchestrationClient starter,
-        //    ILogger log)
-        //{
-        //    // Function input comes from the request content.
-        //    string instanceId = await starter.StartNewAsync("ParamsRead", null);
-
-        //    log.LogInformation("Started orchestration with ID = '{instanceId}'.", instanceId);
-
-        //    return starter.CreateCheckStatusResponse(req, instanceId);
-        //}
     }
 }
